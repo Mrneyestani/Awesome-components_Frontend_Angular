c9cae69a3d34cabef6af9f07995c1bd0845acb38
Reza Neyestani <mrneyestani@gmail.com>
2024-01-08 19:25:59 +0100

    add animation

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,11 +1,32 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {Comment} from '../../../core/models/comment.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        transform: 'scale(1.05)',
+        'background-color': 'rgb(201, 157, 242)',
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ]),
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
 
@@ -14,11 +35,15 @@ export class CommentsComponent implements OnInit {
   @Output() newComment = new EventEmitter<string>();
   
   commentCtrl!: FormControl;
+  animationStates: { [key: number]: 'default' | 'active' } = {};
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
+    for (let index in this.comments) {
+      this.animationStates[index] = 'default';
+  }
   }
   
   onLeaveComment() {
@@ -27,6 +52,14 @@ export class CommentsComponent implements OnInit {
     }
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
+}
+
+onListItemMouseEnter(index: number) {
+  this.animationStates[index] = 'active';
+}
+
+onListItemMouseLeave(index: number) {
+  this.animationStates[index] = 'default';
 }
 
 }
