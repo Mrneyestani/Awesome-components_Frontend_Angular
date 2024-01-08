df8a1aa49f5ebbf4af943bd2b3ea50f684582cb2
Reza Neyestani <mrneyestani@gmail.com>
2024-01-08 10:27:30 +0100

    reusable components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -9,7 +10,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     CommonModule
   ],
   exports:[
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ]
 })
 export class SharedModule { }
