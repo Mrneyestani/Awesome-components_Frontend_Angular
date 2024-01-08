1822396bd703ad42b7d5307428f7a5b03df69e61
Reza Neyestani <mrneyestani@gmail.com>
2024-01-08 22:04:16 +0100

    Add animation complex

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,14 +1,27 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {Comment} from '../../../core/models/comment.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, animateChild, group, query, sequence, stagger, state, style, transition, trigger, useAnimation } from '@angular/animations';
+import { flashAnimation } from '../../animations/flash.animation';
+import { slideAndFadeAnimation } from '../../animations/slide-and-fade.animation';
+
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
   animations: [
-    trigger('listItem', [
+ 
+      trigger('list', [
+        transition(':enter', [
+            query('@listItem', [
+                stagger(50, [
+                    animateChild()
+                ])
+            ])
+        ])
+    ]),
+      trigger('listItem', [
       state('default', style({
         transform: 'scale(1)',
         'background-color': 'white',
@@ -25,18 +38,43 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
       transition('active => default', [
         animate('500ms ease-in-out')
       ]),
-      transition('void => *', [
-        style({
-            transform: 'translateX(-100%)',
-            opacity: 0,
-            'background-color': 'rgb(201, 157, 242)',
+      transition('deleting => *', [
+        animate('200ms')
+      ]),
+      transition('* => deleting', [
+        animate('2000ms ease-out')
+      ]),
+      transition(':enter', [
+        query('.comment-text, .comment-date', [
+          style({
+              opacity: 0
+          })
+      ]),
+        useAnimation(slideAndFadeAnimation, {
+          params: {
+            time: '500ms',
+            startColor: 'rgb(201,157,242)'
+          }
         }),
-        animate('250ms ease-out', style({
-            transform: 'translateX(0)',
-            opacity: 1,
-            'background-color': 'white',
-        }))
-    ])
+        group([
+          useAnimation(flashAnimation, {
+            params: {
+              time: '250ms',
+              flashColor: 'rgb(249,179,111)'
+            }
+          }),
+          query('.comment-text', [
+              animate('250ms', style({
+                  opacity: 1
+              }))
+          ]),
+          query('.comment-date', [
+              animate('500ms', style({
+                  opacity: 1
+              }))
+          ]),
+      ]),
+      ])
     ])
   ]
 })
